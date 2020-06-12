e4c30ffaa309851870efe3b64cc9d5a1ed4dfeb6
Tania <taniameoow@gmail.com>
2020-06-13 02:10:18 +0300

    Updates

---
+++ precache-manifest.a9666e08f3ff19d69bc4875dfbcd4da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02b36fcae3d895ac5d497b695844983",
+    "url": "/bios-sound-diagnostics/index.html"
+  },
+  {
+    "revision": "db51df5c53f6001fe496",
+    "url": "/bios-sound-diagnostics/static/css/main.e6327e15.chunk.css"
+  },
+  {
+    "revision": "3a8c56a190725b06c9c8",
+    "url": "/bios-sound-diagnostics/static/js/2.fc463979.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bios-sound-diagnostics/static/js/2.fc463979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db51df5c53f6001fe496",
+    "url": "/bios-sound-diagnostics/static/js/main.b609d9af.chunk.js"
+  },
+  {
+    "revision": "224f4e30e161ce263811",
+    "url": "/bios-sound-diagnostics/static/js/runtime-main.a4b2e8e3.js"
+  }
+]);
